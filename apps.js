dc6955be90e3a6a499bda6da75040ae5a07f402d
serg-bolshakov <you@example.com>
2022-11-05 14:05:02 +0300

    Понервничал немного

---
+++ apps.js
@@ -0,0 +1,10 @@
+const age = 19;
+const gender = 'МУЖ'; // жен
+
+if (age > 18) {
+    console.log('вам больше 18 лет');
+    } else  if (age < 18) {
+        console.log('вам меньше 18 лет');
+    } else {
+        console.log('вам 18 лет');
+    } 
