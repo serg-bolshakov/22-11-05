4c069d054ccc23021d27dcc6431240284997529b
serg-bolshakov <you@example.com>
2022-11-09 19:44:29 +0300

    Начал заново

--- apps.js
+++ apps.js
@@ -1,14 +1,1 @@
-const age = '123z';
-const gender = 'МУЖ'; // жен
-
-
-
-if (age > 18) {
-    console.log('вам больше 18 лет');
-    } else  if (age < 18) {
-        console.log('вам меньше 18 лет');
-    } else if (age === 18) {
-        console.log('вам 18 лет');
-    } else {
-        console.log ('введите ваш возраст');
-        }
+console.log ('Привет');
