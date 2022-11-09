f0779dd3eadaf933100fe44c23337697992503ae
serg-bolshakov <you@example.com>
2022-11-09 20:17:58 +0300

    Условие if

--- apps.js
+++ apps.js
@@ -1,1 +1,13 @@
-console.log ('Привет всем');
+const age = 18;
+const gender = "Муж";
+
+if (age >18) {
+    console.log ('Вам больше 18 лет');
+}
+
+if (gender.toLocaleLowerCase() === "муж") {
+    console.log ("Вам направо")
+} 
+if (gender.toLocaleLowerCase() === "жен") {
+    console.log ("Вам налево")
+} 
