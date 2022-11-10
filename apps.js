8ff70456bce06c521d31129509d025dbaef36f3e
serg-bolshakov <you@example.com>
2022-11-10 17:47:03 +0300

    Система проверки возраста

--- apps.js
+++ apps.js
@@ -1,11 +1,21 @@
-const age = 19;
+let age;
 const gender = "Муж";
 
-if (age >18) {
-    console.log ('Вам больше 18 лет');
-} else {
-    console.log ("Вам меньше 18 лет")
+age = +age;
+if (!isNaN(age)) {
+    if (age > 18) {
+        console.log ('Вам больше 18 лет');
+    } else if (age < 18) {
+        console.log ("Вам меньше 18 лет");
+    } else if (age == 18) {
+        console.log ("Поздравляем! Вам 18 лет");
+    } else {
+        console.log ("Пожалуйста, укажите ваш возраст");
+    }
 }
+console.log ("Введите корректные данные")
+
+
 
 if (gender.toLocaleLowerCase() === "муж") {
     console.log ("Вам направо")
